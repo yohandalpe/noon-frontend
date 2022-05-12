f70646426303a0b37b4194a51fd5e9d8be2ceaee
Yohan Dalpatadu <yohandalpe@gmail.com>
2022-05-12 22:50:37 +0530

    Added global styled-component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import { GlobalStyles } from "../styles/global-styles";
+import "font-awesome/css/font-awesome.css";
+
+//import "../styles/post.css";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
