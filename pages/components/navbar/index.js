3ce68998081178bf2cd2d516ae82a02e8f2664d4
Yohan Dalpatadu <yohandalpe@gmail.com>
2022-05-12 22:51:47 +0530

    Init NavBar

---
+++ pages/components/navbar/index.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import * as NavBar from "./styles/navbar";
+
+export default function Navbar() {
+  return (
+    <>
+      <NavBar.Container>
+        <Link href="/" passHref>
+          <NavBar.Button>
+            <i className="fa fa-home" />
+          </NavBar.Button>
+        </Link>
+        <Link href="/" passHref>
+          <NavBar.Button>
+            <i className="fa fa-heart" />
+          </NavBar.Button>
+        </Link>
+      </NavBar.Container>
+    </>
+  );
+}
---
+++ pages/components/navbar/styles/navbar.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import breakpoints from "../../../utils/breakpoints";
+
+// Create a Title component that'll render an <h1> tag with some styles
+export const Container = styled.nav`
+width: 100%;
+height: 44px;
+background-color: hsl(0, 0%, 100%);
+
+display: flex;
+position: fixed;
+bottom: 0;
+left: 0;
+z-index: 2;
+&:after {
+  content: '';
+  position: absolute;
+  top: 0;
+
+  width: 100%;
+  height: 1px;
+  background-color: hsl(0, 0%, 86%);
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+export const Button = styled.a`
+  flex: 1 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
