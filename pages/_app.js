21d361110f36848a7a4b97444cdb14cb6fcf9638
Yohan Dalpatadu <yohandalpe@gmail.com>
2022-05-15 00:23:46 +0530

    Created a common layout for pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { GlobalStyles } from "../styles/global-styles";
+import Layout from "./components/layout";
 import "font-awesome/css/font-awesome.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
---
+++ pages/components/layout.js
@@ -0,0 +1,12 @@
+import Navbar from "./navbar";
+
+function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+}
+
+export default Layout;
