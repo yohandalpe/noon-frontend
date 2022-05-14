38d167d0cfa6be6597a5a04ef29e52576c2d5ff1
Yohan Dalpatadu <yohandalpe@gmail.com>
2022-05-15 00:27:55 +0530

    Completed favourites page

--- pages/favourites.js
+++ pages/favourites.js
@@ -1,1 +1,38 @@
-export default function Favourites() {}
+import Seo from "./components/common/seo";
+import Post from "./components/post";
+
+function Favourites({ posts }) {
+  //filter favourite posts
+  const filter = {
+    is_favourite: 1,
+  };
+
+  posts = posts.filter((item) => {
+    for (let key in filter) {
+      if (item[key] === undefined || item[key] != filter[key]) return false;
+    }
+    return true;
+  });
+
+  return (
+    <>
+      <Seo title="Favourites" />
+      <Post posts={posts} />
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const response = await fetch(`http://localhost:3001/api/posts`);
+  const posts = await response.json();
+
+  // Pass data to the page via props
+  return {
+    props: {
+      posts: posts.data,
+    },
+  };
+}
+
+export default Favourites;
