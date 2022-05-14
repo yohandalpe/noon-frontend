70265bc89c434c4e3c6c2a2342cac7a8b7100e02
Yohan Dalpatadu <yohandalpe@gmail.com>
2022-05-15 03:50:39 +0530

    Added navbar links labels

--- pages/components/navbar/index.js
+++ pages/components/navbar/index.js
@@ -4,18 +4,22 @@ import * as NavBar from "./styles/navbar";
 function Navbar() {
   return (
     <>
-      <NavBar.Container>
-        <Link href="/" passHref>
-          <NavBar.Button>
-            <i className="fa fa-home" />
-          </NavBar.Button>
-        </Link>
-        <Link href="/favourites" passHref>
-          <NavBar.Button>
-            <i className="fa fa-heart" />
-          </NavBar.Button>
-        </Link>
-      </NavBar.Container>
+      <NavBar.Wrapper>
+        <NavBar.Container>
+          <Link href="/" passHref>
+            <NavBar.Button>
+              <i className="fa fa-home" />{" "}
+              <span className="nav-label">Home</span>
+            </NavBar.Button>
+          </Link>
+          <Link href="/favourites" passHref>
+            <NavBar.Button>
+              <i className="fa fa-heart" />{" "}
+              <span className="nav-label">Favourites</span>
+            </NavBar.Button>
+          </Link>
+        </NavBar.Container>
+      </NavBar.Wrapper>
     </>
   );
 }
