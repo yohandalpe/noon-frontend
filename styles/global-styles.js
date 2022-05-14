38654822da64560832a0db107994c98f848f1f82
Yohan Dalpatadu <yohandalpe@gmail.com>
2022-05-15 03:51:39 +0530

    CSS updates for responsive UI

--- styles/global-styles.js
+++ styles/global-styles.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import breakpoints from "../pages/utils/breakpoints";
 
 // Declare Global Styles
 export const GlobalStyles = createGlobalStyle`
@@ -6,5 +7,16 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: 'Roboto', sans-serif;
-    background: #ffffff;
-}`;
+    background: rgb(211,211,211);
+  }
+  main {
+      width: 100%;
+      ${breakpoints.sm} {
+        width: 640px;
+      }
+      ${breakpoints.md} {
+        width: 500px;
+      }
+      margin: 0 auto;
+      box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  }`;
