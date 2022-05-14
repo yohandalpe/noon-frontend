49f49da63205b7ae84cf7de9be2530936a682410
Yohan Dalpatadu <yohandalpe@gmail.com>
2022-05-15 03:51:28 +0530

    CSS updates for responsive UI

--- pages/components/navbar/styles/navbar.js
+++ pages/components/navbar/styles/navbar.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 import breakpoints from "../../../utils/breakpoints";
 
-// Create a Container component that'll render an <nav> tag with some styles
-export const Container = styled.nav`
+// Create a Wrapper component that'll render an <nav> tag with some styles
+export const Wrapper = styled.nav`
   width: 100%;
   height: 50px;
   background-color: rgb(255, 255, 255);
-  display: flex;
   position: fixed;
   bottom: 0;
+  top: unset;
   left: 0;
   z-index: 2;
   &:after {
@@ -18,6 +18,24 @@ export const Container = styled.nav`
     width: 100%;
     height: 1px;
     background-color: rgb(219, 219, 219);
+  }
+  ${breakpoints.lg} {
+    top: 0;
+    bottom: unset;
+  }
+`;
+
+// Create a Container component that'll render an <div> tag with some styles
+export const Container = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  margin: 0 auto;
+  ${breakpoints.sm} {
+    width: 640px;
+  }
+  ${breakpoints.md} {
+    width: 500px;
   }
 `;
 
@@ -30,4 +48,12 @@ export const Button = styled.a`
   text-decoration: none;
   color: rgb(0, 0, 0);
   font-size: 20px;
+  span {
+    font-size: 14px;
+    margin-left: 0.5rem;
+    display: none;
+    ${breakpoints.lg} {
+      display: block;
+    }
+  }
 `;
